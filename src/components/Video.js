a01b508d2bf5b127462b7d3a9ac67c96bf628c56
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-30 12:18:23 +0530

    commit

---
+++ src/components/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Video.css";
+
+const Video = () => {
+  return (
+    <div className="video">
+      <video src="https://ak.picdn.net/shutterstock/videos/1035625535/preview/stock-footage-akame-forty-eight-waterfalls-in-japan-wonderful-fresh-water-rapids-waterfalls-river-flowing.webm"></video>
+    </div>
+  );
+};
+
+export default Video;
